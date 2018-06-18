08d4ea02cab56077de0e1ad4747575f5f7560f4c
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-18 13:26:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,7 @@
 # out the current state.
 def display_board(board)
 
-rows[0] = "   |   |   \n"
-rows[1] = "-----------\n"
-rows[2] = "   |   |   \n"
-rows[3] = "-----------\n"
-rows[4] = "   |   |   \n"
+rows= ["   |   |   \n", "-----------\n", "   |   |   \n", "-----------\n", "   |   |   \n"]
 
 end
 
