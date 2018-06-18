0fe05834c7ca6efa69c135c36e28c859048b2242
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-18 13:31:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board(board)
 
-rows= ["   |   |   \n","-----------\n","   |   |   \n","-----------\n","   |   |   \n"]
+rows= ["   |   |   \n""-----------\n""   |   |   \n""-----------\n""   |   |   \n"]
 puts "#{rows[0]},#{rows[1]},#{rows[2]},#{rows[3]},#{rows[4]}"
 
 end
